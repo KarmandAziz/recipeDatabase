e570663ab968779aacb627cde1e8363b93b81f8f
KarmandAziz <Karmand94@gmail.com>
2021-12-08 10:50:34 +0100

    Ingredient entity added

--- src/main/java/com/example/recipeDatabase/model/entity/Ingredient.java
+++ src/main/java/com/example/recipeDatabase/model/entity/Ingredient.java
@@ -1,13 +1,70 @@
 package com.example.recipeDatabase.model.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
+import java.util.Objects;
+
+import static com.example.recipeDatabase.model.constants.EntityConstants.GENERATOR;
+import static com.example.recipeDatabase.model.constants.EntityConstants.UUID_GENERATOR;
 
 @Entity
 public class Ingredient {
 
     @Id
+    @GeneratedValue(generator = GENERATOR)
+    @GenericGenerator(name = GENERATOR, strategy = UUID_GENERATOR)
+    @Column(updatable = false)
     private String id;
+    @Column(unique = true)
     private String ingredient;
 
+    public Ingredient(String id, String ingredient) {
+        this.id = id;
+        this.ingredient = ingredient;
+    }
+
+    public Ingredient() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingredient that = (Ingredient) o;
+        return Objects.equals(ingredient, that.ingredient);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ingredient);
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "id='" + id + '\'' +
+                ", ingredient='" + ingredient + '\'' +
+                '}';
+    }
 }
