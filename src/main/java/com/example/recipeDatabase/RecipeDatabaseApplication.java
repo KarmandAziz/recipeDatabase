4b9a23b25ba9564855d7c6a365c2194312b7d140
KarmandAziz <Karmand94@gmail.com>
2021-12-08 10:36:06 +0100

    first commit

---
+++ src/main/java/com/example/recipeDatabase/RecipeDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipeDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/recipeDatabase/RecipeDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipeDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
