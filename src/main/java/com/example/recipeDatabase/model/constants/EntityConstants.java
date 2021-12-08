b470eeffabf8eea20e834d8fc9a91749421bb412
KarmandAziz <Karmand94@gmail.com>
2021-12-08 10:43:33 +0100

    Constant for ID generator added

---
+++ src/main/java/com/example/recipeDatabase/model/constants/EntityConstants.java
@@ -0,0 +1,6 @@
+package com.example.recipeDatabase.model.constants;
+
+public class EntityConstants {
+    public static final String GENERATOR = "UUID";
+    public static final String UUID_GENERATOR = "org.hibernate.id.UUIDGenerator";
+}
--- src/main/java/com/example/recipeDatabase/model/entity/Ingredient.java
+++ src/main/java/com/example/recipeDatabase/model/entity/Ingredient.java
@@ -1,4 +1,13 @@
 package com.example.recipeDatabase.model.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Ingredient {
+
+    @Id
+    private String id;
+    private String ingredient;
+
 }
