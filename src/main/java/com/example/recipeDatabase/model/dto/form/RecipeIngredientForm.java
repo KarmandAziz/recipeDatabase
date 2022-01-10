dadce0b96d7f6f867be1316fa9ead99d179ecc77
KarmandAziz <Karmand94@gmail.com>
2022-01-10 08:39:42 +0100

    Form added

---
+++ src/main/java/com/example/recipeDatabase/model/dto/form/RecipeIngredientForm.java
@@ -0,0 +1,52 @@
+package com.example.recipeDatabase.model.dto.form;
+
+import com.example.recipeDatabase.model.constants.Measurement;
+import com.example.recipeDatabase.validation.OnPost;
+import com.example.recipeDatabase.validation.OnPut;
+
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+import static com.example.recipeDatabase.validation.message.ValidationMessages.MANDATORY_FIELD;
+
+public class RecipeIngredientForm implements Serializable {
+
+    @NotBlank(message = MANDATORY_FIELD ,groups = OnPost.class)
+    private String id;
+
+    @NotBlank(message = MANDATORY_FIELD, groups = {OnPost.class, OnPut.class})
+    private double amount;
+
+
+    private Measurement measurement;
+
+
+
+    public RecipeIngredientForm() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Measurement getMeasurement() {
+        return measurement;
+    }
+
+    public void setMeasurement(Measurement measurement) {
+        this.measurement = measurement;
+    }
+
+}
