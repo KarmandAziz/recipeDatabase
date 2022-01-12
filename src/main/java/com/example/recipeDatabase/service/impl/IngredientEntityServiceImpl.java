7fef27ea165ab891ef044628ed82c84d628940a3
KarmandAziz <Karmand94@gmail.com>
2022-01-12 16:17:51 +0100

    IngredientEntityServiceImpl added

--- src/main/java/com/example/recipeDatabase/service/impl/IngredientEntityServiceImpl.java
+++ src/main/java/com/example/recipeDatabase/service/impl/IngredientEntityServiceImpl.java
@@ -1,40 +1,68 @@
 package com.example.recipeDatabase.service.impl;
 
+import com.example.recipeDatabase.data.IngredientDAO;
+import com.example.recipeDatabase.data.RecipeIngredientDAO;
+import com.example.recipeDatabase.exception.AppResourceNotFoundException;
 import com.example.recipeDatabase.model.dto.form.IngredientForm;
 import com.example.recipeDatabase.model.entity.Ingredient;
-import com.example.recipeDatabase.service.GenericEntityService;
 import com.example.recipeDatabase.service.IngredientEntityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 @Transactional
 public class IngredientEntityServiceImpl implements IngredientEntityService {
 
+    private final IngredientDAO ingredientDAO;
+    private final RecipeIngredientDAO recipeIngredientDAO;
+
+    @Autowired
+    public IngredientEntityServiceImpl(IngredientDAO ingredientDAO, RecipeIngredientDAO recipeIngredientDAO) {
+        this.ingredientDAO = ingredientDAO;
+        this.recipeIngredientDAO = recipeIngredientDAO;
+    }
+
 
     @Override
     public Ingredient create(IngredientForm ingredientForm) {
-        return null;
+        if(ingredientForm == null){
+            throw new IllegalArgumentException("IngredientForm was null");
+        }
+        Ingredient ingredient = new Ingredient();
+        ingredient.setIngredientName(ingredientForm.getIngredientName());
+
+        return ingredientDAO.save(ingredient);
     }
 
     @Override
     public Ingredient findById(String id) {
-        return null;
+        return ingredientDAO.findById(id)
+                .orElseThrow(() -> new AppResourceNotFoundException("Could not find Ingredient with id " +id));
     }
 
     @Override
     public List<Ingredient> findAll() {
-        return null;
+        return ingredientDAO.findAll();
     }
 
     @Override
     public Ingredient update(String id, IngredientForm ingredientForm) {
-        return null;
+        Ingredient ingredient = findById(id);
+        ingredient.setIngredientName(ingredientForm.getIngredientName());
+        ingredient.setId(ingredientForm.getId());
+
+        return ingredientDAO.save(ingredient);
+
     }
 
     @Override
     public void delete(String id) {
+        Ingredient ingredient = findById(id);
+        ingredientDAO.delete(ingredient);
 
     }
 }
