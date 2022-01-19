fe982b1fbf95cca4e103ed2e4a4e0dbca967b591
KarmandAziz <Karmand94@gmail.com>
2022-01-19 21:35:17 +0100

    JWTTokenValidatorFilter & JWTUtil implemented

---
+++ src/main/java/com/example/recipeDatabase/security/JWTTokenValidatorFilter.java
@@ -0,0 +1,55 @@
+package com.example.recipeDatabase.security;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.example.recipeDatabase.security.SecurityConstants.AUTHORIZATION;
+import static com.example.recipeDatabase.security.SecurityConstants.BEARER;
+
+public class JWTTokenValidatorFilter extends BasicAuthenticationFilter {
+
+    private final JWTUtil jwtUtil;
+
+    public JWTTokenValidatorFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+        super(authenticationManager);
+        this.jwtUtil = jwtUtil;
+    }
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String jwt = request.getHeader(AUTHORIZATION);
+        if(jwt != null){
+            try{
+                if(!jwt.startsWith(BEARER)){
+                    throw new BadCredentialsException("Invalid token");
+                }
+                jwt = jwt.substring(BEARER.length());
+                Claims claims = jwtUtil.parseClaims(jwt);
+                AppUserDetails appUserDetails = jwtUtil.fromClaims(claims);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(
+                        appUserDetails, null, appUserDetails.getAuthorities()
+                );
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }catch (Exception e){
+                throw new BadCredentialsException("Invalid token received");
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return request.getServletPath().equals("/api/v1/public/auth");
+    }
+}
---
+++ src/main/java/com/example/recipeDatabase/security/JWTUtil.java
@@ -0,0 +1,92 @@
+package com.example.recipeDatabase.security;
+
+import com.example.recipeDatabase.model.entity.AppRole;
+import com.example.recipeDatabase.model.entity.AppUser;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static com.example.recipeDatabase.security.SecurityConstants.*;
+@Component
+public class JWTUtil {
+
+    public Claims parseClaims(String jwt) {
+        SecretKey key = Keys.hmacShaKeyFor(JWT_KEY.getBytes(StandardCharsets.UTF_8));
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody();
+    }
+
+    public AppUserDetails fromClaims(Claims claims){
+        String username = claims.getSubject();
+        String userId = claims.get(USER_ID, String.class);
+        String authorities = claims.get(AUTHORITIES, String.class);
+        Set<SimpleGrantedAuthority> authoritySet = Arrays.stream(authorities.split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+        AppUserDetails appUserDetails = new AppUserDetails();
+        appUserDetails.setUserId(userId);
+        appUserDetails.setAuthorities(authoritySet);
+        appUserDetails.setUsername(username);
+        return appUserDetails;
+    }
+
+    public String fromAppUserDetails(AppUserDetails appUserDetails){
+        SecretKey key = Keys.hmacShaKeyFor(JWT_KEY.getBytes(StandardCharsets.UTF_8));
+        return Jwts.builder()
+                .setIssuer("recipe-database")
+                .setHeaderParam("type", "JWT")
+                .setSubject(appUserDetails.getUsername())
+                .claim(AUTHORITIES, populateAuthorities(appUserDetails.getAuthorities()))
+                .claim(USER_ID, appUserDetails.getUserId())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(
+                        System.currentTimeMillis() + 3_600_000
+                ))
+                .signWith(key, SignatureAlgorithm.HS512).compact();
+    }
+
+    public String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        Set<String> authoritiesSet = new HashSet<>();
+        for(GrantedAuthority authority : authorities){
+            authoritiesSet.add(authority.getAuthority());
+        }
+        return String.join(",", authoritiesSet);
+    }
+
+    public String populateAuthorities(AppUser appUser){
+        Set<String> authorities = new HashSet<>();
+        for(AppRole appRole : appUser.getRoles()){
+            authorities.add(appRole.getUserRole().name());
+        }
+        return String.join(",", authorities);
+    }
+
+    public String fromAppUser(AppUser appUser){
+        SecretKey key = Keys.hmacShaKeyFor(JWT_KEY.getBytes(StandardCharsets.UTF_8));
+        return Jwts.builder()
+                .setIssuer("recipe-database")
+                .setHeaderParam("type", "JWT")
+                .setSubject(appUser.getUsername())
+                .claim(AUTHORITIES, populateAuthorities(appUser))
+                .claim(USER_ID, appUser.getId())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(
+                        System.currentTimeMillis() + 3_600_000
+                ))
+                .signWith(key, SignatureAlgorithm.HS512).compact();
+    }
+
+
+}
