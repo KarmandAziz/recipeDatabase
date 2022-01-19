b1c8d1b14387c015140420c0063be08900f8629a
KarmandAziz <Karmand94@gmail.com>
2022-01-19 15:51:53 +0100

    AppUserController added

--- src/main/java/com/example/recipeDatabase/service/facade/implementations/RecipeServiceImpl.java
+++ src/main/java/com/example/recipeDatabase/service/facade/implementations/RecipeServiceImpl.java
@@ -9,11 +9,14 @@ import com.example.recipeDatabase.service.facade.interfaces.RecipeService;
 import com.example.recipeDatabase.service.interfaces.RecipeCategoryEntityService;
 import com.example.recipeDatabase.service.interfaces.RecipeEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-
+@Service
+@Transactional
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeEntityService recipeEntityService;
