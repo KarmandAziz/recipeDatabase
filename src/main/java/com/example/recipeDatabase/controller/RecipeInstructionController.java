b71769cd491f2b716ba249183894cf5e2390d53c
KarmandAziz <Karmand94@gmail.com>
2022-01-19 16:11:50 +0100

    RecipeController added

---
+++ src/main/java/com/example/recipeDatabase/controller/RecipeInstructionController.java
@@ -0,0 +1,54 @@
+package com.example.recipeDatabase.controller;
+
+import com.example.recipeDatabase.model.dto.form.RecipeInstructionForm;
+import com.example.recipeDatabase.model.dto.view.RecipeInstructionDTO;
+import com.example.recipeDatabase.service.facade.interfaces.RecipeInstructionService;
+import com.example.recipeDatabase.validation.OnPost;
+import com.example.recipeDatabase.validation.OnPut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class RecipeInstructionController {
+
+    private final RecipeInstructionService recipeInstructionService;
+
+    @Autowired
+    public RecipeInstructionController(RecipeInstructionService recipeInstructionService) {
+        this.recipeInstructionService = recipeInstructionService;
+    }
+
+    @PostMapping("/api/v1/instructions")
+    public ResponseEntity<RecipeInstructionDTO> create(@Validated(OnPost.class) @RequestBody RecipeInstructionForm recipeInstructionForm) {
+        RecipeInstructionDTO instructions = recipeInstructionService.create(recipeInstructionForm);
+        return ResponseEntity.status(201).body(instructions);
+    }
+
+
+    @GetMapping("/api/v1/instructions")
+    public ResponseEntity<List<RecipeInstructionDTO>> find() {
+        return ResponseEntity.ok(
+                recipeInstructionService.findAll()
+        );
+    }
+
+    @GetMapping("/api/v1/instructions/{id}")
+    public ResponseEntity<RecipeInstructionDTO> findById(@PathVariable("id") String id) {
+        return ResponseEntity.ok(recipeInstructionService.findById(id));
+    }
+
+    @PutMapping("/api/v1/instructions/{id}")
+    public ResponseEntity<RecipeInstructionDTO> update(@PathVariable("id") String id, @Validated(OnPut.class) @RequestBody RecipeInstructionForm form) {
+        return ResponseEntity.ok(recipeInstructionService.update(id, form));
+    }
+
+    @DeleteMapping("/api/v1/instructions/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") String id) {
+        recipeInstructionService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
